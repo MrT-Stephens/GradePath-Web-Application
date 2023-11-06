7e3cb59ed9d9342306add507812aee6db88167d2
MrT-Stephens <92452307+MrT-Stephens@users.noreply.github.com>
2023-11-06 15:23:46 +0000

    Added fetch to login page

--- app/login/page.js
+++ app/login/page.js
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { metadata } from '../layout'
+import LoginForm from './login-form'
 
 export default function Page() {
     return (
@@ -12,25 +13,7 @@ export default function Page() {
                     <h1 className="text-6xl font-bold text-center text-black dark:text-white mb-10">
                         Login
                     </h1>
-                    <form className="flex flex-col items-center justify-center space-y-5">
-                        <input type="email" className="w-80 h-10 rounded-xl text-black p-2" placeholder="Email" />
-                        <input type="password" className="w-80 h-10 rounded-xl text-black p-2" placeholder="Password" />
-                        <button type="submit" className="group w-80 h-10 bg-gradient-to-r from-green-400 to-blue-500 text-black dark:text-white font-bold rounded-xl">
-                            Login {' '}
-                            <span className="inline-block transition-transform group-hover:translate-x-5 motion-reduce:transform-none">
-                                -&gt;
-                            </span>
-                        </button>
-                        <h6 className="text-center text-gray-400">
-                            Don't have an account?
-                        </h6>
-                        <a href="/register" className="group flex items-center justify-center w-80 h-10 bg-gradient-to-r from-green-400 to-blue-500 text-black dark:text-white font-bold rounded-xl">
-                            Create your GradePath account {' '}
-                            <span className="inline-block transition-transform group-hover:translate-x-5 motion-reduce:transform-none">
-                                -&gt;
-                            </span>
-                        </a>
-                    </form>
+                    <LoginForm />
                 </div>
             </div>
         </main>
