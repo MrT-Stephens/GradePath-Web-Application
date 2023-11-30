9793e7686c800c4268dd82d65e87a78db1807b01
MrT-Stephens <MrTstephens123@gmail.com>
2023-11-30 12:25:24 +0000

    Fixed register page. From database update

--- app/register/register-form.js
+++ app/register/register-form.js
@@ -5,11 +5,16 @@ import PocketBase from 'pocketbase'
 import { useRouter } from 'next/navigation'
 
 export default function Register() {
+    const [userFName, setUserFName] = useState('');
+    const [userLName, setUserLName] = useState('');
+    const [userDoB, setUserDoB] = useState('');
+    const [userAddressLine1, setUserAddressLine1] = useState('');
+    const [userAddressLine2, setUserAddressLine2] = useState('');
+    const [userCity, setUserCity] = useState('');
+    const [userPostcode, setUserPostcode] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirm, setConfirm] = useState('');
-    const [firstname, setFirstname] = useState('');
-    const [lastname, setLastname] = useState('');
 
     const pbClient = new PocketBase('http://localhost:8090');
     const router = useRouter();
@@ -17,35 +22,51 @@ export default function Register() {
     const handelSubmit = async (e) => {
         e.preventDefault();
 
-        await pbClient.collection('users').create({
-            "email": email,
-            "emailVisibility": true,
-            "password": password,
-            "passwordConfirm": confirm,
-            "firstname": firstname,
-            "lastname": lastname
-        }).then((response) => {
-            setEmail('');
-            setPassword('');
-            setConfirm('');
-            setFirstname('');
-            setLastname('');
-            router.push(`/user/${response.id}`);
-        }).catch((error) => {
+        var response;
+
+        try {
+            response = await pbClient.collection('users').create({
+                "email": email,
+                "emailVisibility": true,
+                "password": password,
+                "passwordConfirm": confirm,
+                "userFName": userFName,
+                "userLName": userLName,
+                "userDoB": userDoB,
+                "userAddressLine1": userAddressLine1,
+                "userAddressLine2": userAddressLine2,
+                "userCity": userCity,
+                "userPostcode": userPostcode
+            });
+        }
+        catch (error) {
             alert(error);
+            setUserFName('');
+            setUserLName('');
+            setUserDoB('');
+            setUserAddressLine1('');
+            setUserAddressLine2('');
+            setUserCity('');
+            setUserPostcode('');
             setEmail('');
             setPassword('');
             setConfirm('');
-            setFirstname('');
-            setLastname('');
             console.log(error);
-        });
+            return;
+        }
+
+        router.push(`/user/${response.id}`);
     }
 
     return (
         <form className="flex flex-col items-center justify-center space-y-5" onSubmit={handelSubmit}>
-            <input type="text" className="w-80 h-10 rounded-xl text-black p-2" placeholder="First Name" value={firstname} onChange={(e) => setFirstname(e.target.value)} />
-            <input type="text" className="w-80 h-10 rounded-xl text-black p-2" placeholder="Last Name" value={lastname} onChange={(e) => setLastname(e.target.value)} />
+            <input type="text" className="w-80 h-10 rounded-xl text-black p-2" placeholder="First Name" value={userFName} onChange={(e) => setUserFName(e.target.value)} />
+            <input type="text" className="w-80 h-10 rounded-xl text-black p-2" placeholder="Last Name" value={userLName} onChange={(e) => setUserLName(e.target.value)} />
+            <input type="data" className="w-80 h-10 rounded-xl text-black p-2" placeholder="Date of Birth" value={userDoB} onChange={(e) => setUserDoB(e.target.value)} />
+            <input type="text" className="w-80 h-10 rounded-xl text-black p-2" placeholder="Address Line 1" value={userAddressLine1} onChange={(e) => setUserAddressLine1(e.target.value)} />
+            <input type="text" className="w-80 h-10 rounded-xl text-black p-2" placeholder="Address Line 2" value={userAddressLine2} onChange={(e) => setUserAddressLine2(e.target.value)} />
+            <input type="text" className="w-80 h-10 rounded-xl text-black p-2" placeholder="City" value={userCity} onChange={(e) => setUserCity(e.target.value)} />
+            <input type="text" className="w-80 h-10 rounded-xl text-black p-2" placeholder="Postcode" value={userPostcode} onChange={(e) => setUserPostcode(e.target.value)} />
             <input type="email" className="w-80 h-10 rounded-xl text-black p-2" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
             <input type="password" className="w-80 h-10 rounded-xl text-black p-2" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
             <input type="password" className="w-80 h-10 rounded-xl text-black p-2" placeholder="Confirm Password" value={confirm} onChange={(e) => setConfirm(e.target.value)} />
