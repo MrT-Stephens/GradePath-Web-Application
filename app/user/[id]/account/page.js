aff22f73f710f8ed5502dfef54f8b028466f0e35
ViewtifulJim <jamescolinwilliams@outlook.com>
2023-11-30 13:38:34 +0000

    Added hello 'id' to the left side of the nav bar

--- app/user/[id]/account/page.js
+++ app/user/[id]/account/page.js
@@ -1,19 +1,26 @@
 import Image from 'next/image';
 import { metadata } from '../../../layout';
 
-export default function Page() {
+async function getUserName(userId) {
+    const pocketbase = new PocketBase('http://127.0.0.1:8090');
+
+    const user = await pocketbase.collection('users').getOne(userId);
+
+    return `${user.userFName} ${user.userLName}`;
+}
+
+export default async function Page({ params }) {
+    const { id } = params;
+
+    const userName = (id === 'guest') ? 'Guest' : await getUserName(id);
+
     return (
         <div>
             {/* Navigation Bar */}
             <nav className="bg-gradient-to-r from-black-400 to-black-500 p-4 grid grid-cols-3">
                 {/* Left Navbar buttons */}
                 <div className="flex items-center">
-                    <button className="mr-2 text-white font-bold py-2 px-4 rounded bg-blue-500 hover:bg-blue-600">
-                        Accessibility
-                    </button>
-                    <button className="text-white font-bold py-2 px-4 rounded bg-blue-500 hover:bg-blue-600">
-                        Policies
-                    </button>
+                    <h1 className="text-white font-bold py-2 px-4 rounded bg-blue-500">Hello, {userName}!</h1>
                 </div>
 
                 {/* Centered Logo */}
