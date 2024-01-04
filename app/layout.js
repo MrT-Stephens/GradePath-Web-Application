4a10b09ab2b06ce31ed460d85fc8ca1dec3e07e4
MrT-Stephens <92452307+MrT-Stephens@users.noreply.github.com>
2024-01-04 19:36:18 +0000

    Updated metadata

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
     title: 'GradePath',
     slogan: 'THE PATH TO SUCESS',
-    description: 'GradePath is a platform that helps students find the best path to success.',
+    description: 'GradePath is a platform that helps students find their best path to success.',
     authors: [{ name: 'Thomas Stephens' }, { name: 'Mathew Mann' }, { name: 'James Williams' }],
     icon: '/GradePathLogo.svg',
 }
